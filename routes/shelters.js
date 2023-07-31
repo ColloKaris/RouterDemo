df0209bb7f3df901b4ed99ca60fb9a220e4982b3
Collins Kariuki <collinsmunkary@gmail.com>
2023-07-31 17:09:25 +0300

    Express Router Intro

---
+++ routes/shelters.js
@@ -0,0 +1,23 @@
+// Require express
+const express = require('express');
+
+// call express.Router
+const router = express.Router();
+
+router.get('/', (req,res) => {
+    res.send("ALL SHELTERS")
+})
+
+router.post('/', (req,res) => {
+    res.send("CREATING SHELTER")
+})
+
+router.get('/:id', (req,res) => {
+    res.send("VIEWING ONE SHELTER")
+})
+
+router.get('/:id/edit', (req,res) => {
+    res.send("EDITING ONE SHELTER")
+})
+
+module.exports = router;
